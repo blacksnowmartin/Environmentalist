308b3421ae84fe2099f5560d3ea1029cc61d9153
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-11-14 15:23:11 +0300

    Create recycling_lithium-ion_batteries.py
    This script I have written defines a LithiumBattery class with methods to discharge the battery and recycle it. The recycle method simulates the recycling process by setting the charge level to 0. You can integrate this script into a larger recycling program or use it as a starting point for more advanced features.

---
+++ recycling_lithium-ion_batteries.py
@@ -0,0 +1,26 @@
+class LithiumBattery:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.charge_level = 100  # Initial charge level
+
+    def discharge(self, amount):
+        if self.charge_level >= amount:
+            self.charge_level -= amount
+            print(f"Discharged {amount}% from the battery. Current charge: {self.charge_level}%")
+        else:
+            print("Insufficient charge to discharge.")
+
+    def recycle(self):
+        # Simulate the recycling process
+        self.charge_level = 0
+        print("Recycling the lithium battery. Thank you for contributing to recycling efforts!")
+
+# Example usage
+battery = LithiumBattery(capacity=3000)  # Assume a battery with a capacity of 3000 mAh
+
+# Simulate usage
+battery.discharge(20)
+battery.discharge(30)
+
+# Recycling the battery
+battery.recycle()
