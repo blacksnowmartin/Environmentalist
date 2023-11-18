0691d30108deff788ae34a2a7ba678fe70b2e0d3
Martin Kitonga <119602263+blacksnowmartin@users.noreply.github.com>
2023-11-18 19:02:57 +0300

    Create Carbon-print_tracker-folder_structure.lua
    Layout of the folders and subfolders of the project

---
+++ Carbon-print_tracker.lua
@@ -0,0 +1,16 @@
+carbon-footprint-app/
+|-- client/
+|   |-- src/
+|   |   |-- components/
+|   |   |-- App.js
+|   |   |-- index.js
+|   |-- package.json
+|-- server/
+|   |-- src/
+|   |   |-- controllers/
+|   |   |-- models/
+|   |   |-- routes/
+|   |   |-- app.js
+|   |   |-- server.js
+|   |-- package.json
+|-- package.json
